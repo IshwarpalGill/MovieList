ed2b15adb273c094effc3885fd4527afdd6d1448
Ishwarpal <ishwarpal.gill@gmail.com>
2019-11-19 15:25:20 -0500

    MovieList with validation and using foreach loop

---
+++ MovieList/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> movieList = new List<Movie>()
+            {
+                {new Movie("Toy Story", "animated") },
+                {new Movie("Coco", "animated") },
+                {new Movie("Wall-E", "animated") },
+                {new Movie("John Q", "drama") },
+                {new Movie("Memiors of a Geisha", "drama") },
+                {new Movie("Get Out", "horror") },
+                {new Movie("Us", "horror") },
+                {new Movie("IT", "horror") },
+                {new Movie("Interstellar", "scifi") },
+                {new Movie("Star Wars", "scifi") }
+            };
+
+            Console.WriteLine("Welcome to the Movie List Application!");
+            Console.WriteLine("What category are you interested in? (Animated, Drama, Horror, Scifi)");
+            string input = Console.ReadLine().ToLower();
+            string ans = Validate(input);
+
+            foreach (Movie m in movieList)
+            {
+                //input = m.Category.ToLower();
+
+                if (m.Category == ans)
+                {
+                    Console.WriteLine($"{m.Title}");
+                }
+            }
+
+        }
+
+        public static string Validate(string input)
+        {
+            //bool again = true;
+            while (true)
+            {
+                try
+                {
+                    if (input == "drama" || input == "animated" || input == "scifi" || input == "horror")
+                    {
+                        //again = false;
+                        return input;
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid category");
+                        input = Console.ReadLine();
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Please enter a valid category");
+                    input = Console.ReadLine();
+                    //again = true;
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine("Please enter a valid category");
+                    input = Console.ReadLine();
+                    //again = true;
+                }
+            }
+            //again = true;
+        }
+    }
+}
